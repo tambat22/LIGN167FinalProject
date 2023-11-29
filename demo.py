544e6b6b2955e8d6f72281fe26ef409547787c0b
kchen283 <kchen1270@gmail.com>
2023-11-28 22:11:13 -0800

    added .env and assistant API integration

--- demo.py
+++ demo.py
@@ -1,22 +1,59 @@
 import streamlit as st
-from openai import OpenAI 
+from openai import OpenAI
+import openai
 import os
 from dotenv import load_dotenv, find_dotenv
+import time
+
+#run 'source .venv/bin/activate' on mac
 
 #load .env file 
-load_dotenv(find_dotenv())
+load_dotenv()
 
 # Set your OpenAI API key
-client = OpenAI(api_key = os.environ.get("OPENAI_API_KEY"))
+client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
 st.title("GPT-based Chatbot")
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a tutor and an assistant to help with test/quiz generation for an upper division intro to linguistics class (LIGN 101) taught at UCSD."},
-    {"role": "user", "content": "What is language?"}
-  ]
+assistant = client.beta.assistants.retrieve("asst_BnvVyG8g4UpskXZzUPl8jIvp")
+
+thread = client.beta.threads.create()
+
+message = client.beta.threads.messages.create(
+    thread_id=thread.id,
+    role="user",
+    content="What topics were covered in the lecture for week 2: what is language?"
 )
 
-print(completion.choices[0].message)
+run = client.beta.threads.runs.create(
+  thread_id=thread.id,
+  assistant_id="asst_BnvVyG8g4UpskXZzUPl8jIvp",
+  instructions="Please address the user as User."
+)
+
+run_completed = False
+
+print ("working")
+
+while not run_completed:
+    # Retrieve the run status
+    run_status = client.beta.threads.runs.retrieve(
+        thread_id=thread.id,
+        run_id=run.id
+    )
+
+    # Check if the run has completed
+    if run_status.status == "completed":  # Replace 'completed' with the actual attribute name
+        run_completed = True
+    else:
+        # Wait for a short period before checking again
+        time.sleep(1)  # Wait for 1 second
+
+# Once the run is complete, retrieve and print messages
+messages = client.beta.threads.messages.list(
+    thread_id=thread.id
+)
+
+for message in messages:
+    print(message)
+
