5cf9ecc9f2150c1ba6d88041ee12dd4b68fe2706
kchen283 <kchen1270@gmail.com>
2023-12-02 18:12:00 -0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ import streamlit as st
 from openai import OpenAI
 import openai
 import os
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv
 import time
 
 #run 'source .venv/bin/activate' on mac
@@ -21,17 +21,20 @@ def generate_api(questions, content, start_week, end_week):
     message = client.beta.threads.messages.create(
         thread_id=thread.id,
         role="user",
-        content="Generate a " + questions + " question " + content + " that covers content from week " + start_week + "to " + "week " + end_week
+        content="Generate a multiple choice " + content + " with only " + questions + " questions using the Lectures.md file from week " + start_week + " to " + " week " + end_week + ". Format it as a test handout with questions and option choices in a new line, and answers at the bottom labeled Answers."
     )
 
     run = client.beta.threads.runs.create(
-    thread_id=thread.id,
-    assistant_id="asst_BnvVyG8g4UpskXZzUPl8jIvp"
+        thread_id=thread.id,
+        assistant_id="asst_BnvVyG8g4UpskXZzUPl8jIvp"
     )
 
-    run_completed = False
+    run_status = client.beta.threads.runs.retrieve(
+        thread_id=thread.id,
+        run_id=run.id
+    )
 
-    while not run_completed:
+    while run_status.status!= "completed":
         # Retrieve the run status
         run_status = client.beta.threads.runs.retrieve(
             thread_id=thread.id,
@@ -39,13 +42,10 @@ def generate_api(questions, content, start_week, end_week):
         )
         print(run_status.status)
 
-        # Check if the run has completed
-        if run_status.status == "completed":  # Replace 'completed' with the actual attribute name
-            run_completed = True
-        else:
-            # Wait for a short period before checking again
-            time.sleep(2)  # Wait for 1 second
+        time.sleep(2)  # Wait for 1 second
+        
         if (run_status.status == "failed"):
+            print("failed")
             break
 
     # Once the run is complete, retrieve and print messages
@@ -54,7 +54,17 @@ def generate_api(questions, content, start_week, end_week):
     )
 
     #print(messages.data[0].content[0].text.value)
-    st.write(messages.data[0].content[0].text.value)
+    # Split the text into questions and answers
+    parts = content.split("Answers:")
+
+    questions_text = parts[0]
+    answers_text = parts[1] if len(parts) > 1 else ""
+
+    print(questions_text)
+    print(answers_text)
+
+    st.write(questions_text)
+    return (questions_text,answers_text)
 
 
 def main():
@@ -118,11 +128,16 @@ def display_quiz_details():
     start_week_quiz = st.number_input("Start Week for Quiz", min_value=1, max_value=10, key='start_week_quiz')
     end_week_quiz = st.number_input("End Week for Quiz", min_value=start_week_quiz, max_value=10, key='end_week_quiz')
     
+    generate_content_message = st.empty()
+
     if (num_questions_quiz and start_week_quiz and end_week_quiz and 
         start_week_quiz <= end_week_quiz and num_questions_quiz <= 100 and end_week_quiz <= 10 and start_week_quiz >= 0):
         if st.button("Confirm Quiz Details", key='confirm_quiz'):
-            st.write(f"Quiz with {num_questions_quiz} question(s) covering weeks {start_week_quiz} to {end_week_quiz} is being generated...")
-            generate_api(str(num_questions_quiz), "Quiz", str(start_week_quiz), str(end_week_quiz))
+            generate_content_message.write(f"Quiz with {num_questions_quiz} question(s) covering weeks {start_week_quiz} to {end_week_quiz} is being generated...")
+            content = generate_api(str(num_questions_quiz), "Quiz", str(start_week_quiz), str(end_week_quiz))
+            generate_content_message.empty()
+            generate_txt_file("quiz",content[0])
+            generate_txt_file("answers","Answers: " + content[1])
 
 def display_test_details():
     st.title("Test Details")
@@ -130,12 +145,23 @@ def display_test_details():
     start_week_test = st.number_input("Start Week for Test", min_value=1, max_value=10, key='start_week_test')
     end_week_test = st.number_input("End Week for Test", min_value=start_week_test, max_value=10, key='end_week_test')
     
+    generate_content_message = st.empty()
+
     if (num_questions_test and start_week_test and end_week_test and 
         start_week_test <= end_week_test and num_questions_test <= 100 and end_week_test <= 10 and start_week_test >= 0):
         if st.button("Confirm Test Details", key='confirm_test'):
-            st.write(f"Test with {num_questions_test} question(s) covering weeks {start_week_test} to {end_week_test} is being generated...")
-            generate_api(str(num_questions_test), "Test", str(start_week_test), str(end_week_test))
+            generate_content_message.write(f"Test with {num_questions_test} question(s) covering weeks {start_week_test} to {end_week_test} is being generated...")
+            content = generate_api(str(num_questions_test), "Test", str(start_week_test), str(end_week_test))
+            generate_content_message.empty()
+            generate_txt_file("test",content[0])
+            generate_txt_file("answers","Answers: " + content[1])
 
+def generate_txt_file(type,text):
+        btn = st.download_button(
+        label="Download " +type+ " as a .txt file",
+        data=text,
+        file_name=type+".txt",
+        mime="text/plain")
 
 if __name__ == "__main__":
     main()
